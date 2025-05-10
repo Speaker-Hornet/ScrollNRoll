a6c0284344fcfff185939b8a8e45d0c251546d5c
Rista <90570031+Ristic-Lazar-smd@users.noreply.github.com>
2025-05-10 20:19:52 +0200

    no message

--- ScrollNRoll/Assets/GunRaycast.cs
+++ ScrollNRoll/Assets/GunRaycast.cs
@@ -65,31 +65,3 @@ public GameObject newEnemyButton;
         Instantiate(testCube, spawnPosition, spawnRotation);
     }
 }
-
-
-
-    // public RectTransform cursorRectTransform;
-    // public float cursorSpeed = 1f;
-
-    // private Vector2 currentPos;
-
-    // void Start()
-    // {
-    //     // Start in center of screen
-    //     currentPos = new Vector2(Screen.width / 2f, Screen.height / 2f);
-    //     Cursor.visible = false; // Hide system cursor
-    // }
-
-    // void Update()
-    // {
-    //     float mouseX = Input.GetAxis("Mouse X");
-    //     float mouseY = Input.GetAxis("Mouse Y");
-
-    //     currentPos += new Vector2(mouseX, mouseY) * cursorSpeed;
-
-    //     // Clamp to screen bounds if you want
-    //     currentPos.x = Mathf.Clamp(currentPos.x, 0, Screen.width);
-    //     currentPos.y = Mathf.Clamp(currentPos.y, 0, Screen.height);
-
-    //     cursorRectTransform.position = currentPos;
-    // }
