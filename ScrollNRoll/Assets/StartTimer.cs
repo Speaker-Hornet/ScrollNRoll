406116c32a9fb4cda25905c36ac7436239cb53a0
Speaker-Hornet <fridmanaleksandar@gmail.com>
2025-05-11 15:33:42 +0200

    Start UI

---
+++ ScrollNRoll/Assets/StartTimer.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class StartTimer : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+ public void StartTheTime()
+    {
+        //POZOVI STA TREBA
+    }
+}
